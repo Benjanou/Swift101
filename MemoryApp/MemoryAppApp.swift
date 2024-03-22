1d94b95d3e92bb29dbd1def45fc6c973f85237f3
Gil Benjano <gil.benjano@playstudios-il.com>
2024-03-22 20:26:36 +0200

    Initial Commit

---
+++ MemoryApp/MemoryAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemoryAppApp.swift
+//  MemoryApp
+//
+//  Created by Gil Benjano on 22/03/2024.
+//
+
+import SwiftUI
+
+@main
+struct MemoryAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
